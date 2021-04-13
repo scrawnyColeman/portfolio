db7a329245a532b7bea1b7ce0b77457eaf33e931
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-13 22:07:54 +0100

    updated technologies array

---
+++ src/data/technologies.js
@@ -0,0 +1,50 @@
+import es6 from "../assets/icons/es6.png";
+import jest from "../assets/icons/jest.png";
+import js from "../assets/icons/js.png";
+import react from "../assets/icons/react.png";
+import rtl from "../assets/icons/rtl.png";
+import sass from "../assets/icons/sass.png";
+import springboot from "../assets/icons/springboot.png";
+import styledcomponents from "../assets/icons/styledcomponents.png";
+import ts from "../assets/icons/ts.png";
+
+const technologies = [
+  {
+    name: "ES6",
+    icon: es6
+  },
+  {
+    name: "Jest",
+    icon: jest
+  },
+  {
+    name: "JavaScript",
+    icon: js
+  },
+  {
+    name: "React",
+    icon: react
+  },
+  {
+    name: "Testing Library",
+    icon: rtl
+  },
+  {
+    name: "SASS",
+    icon: sass
+  },
+  {
+    name: "Spring Boot",
+    icon: springboot
+  },
+  {
+    name: "Styled-Components",
+    icon: styledcomponents
+  },
+  {
+    name: "Typescript",
+    icon: ts
+  }
+];
+
+export default technologies;
