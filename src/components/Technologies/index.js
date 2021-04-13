3715afca2aa2bbe6ae83dceccdff95dc6fa2fa1f
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-13 22:09:02 +0100

    displayed technologies

--- src/components/Technologies/index.js
+++ src/components/Technologies/index.js
@@ -1,22 +1,16 @@
 import "./style.scss";
 import Widget from "../UI/Widget";
+import technologies from "../../data/technologies";
 
 const Technologies = () => {
   return (
     <div className="technologies-container">
       <Widget title="Technologies">
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
-        <div>Technology</div>
+        <div className="icons-wrapper">
+          {technologies.map(tech => (
+            <img src={tech.icon} alt={tech.name} height="48" className="icon" />
+          ))}
+        </div>
       </Widget>
     </div>
   );
