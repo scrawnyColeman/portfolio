053ca2d54ef43530adb9302c65a1478ef2b0e6d0
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-13 19:38:07 +0100

    Improved responsiveness

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,11 @@ import "./style.scss";
 const App = () => {
   return (
     <div className="app-container">
-      <div className="display-image-container">Hi</div>
-      <div className="profile-container">Hello</div>
+      <div className="display-image-container widget">Hi</div>
+      <div className="profile-container widget">Hello</div>
       <div className="technologies-container">Hello</div>
-      <div className="projects-container">Hello</div>
-      <div className="education-container">Hello</div>
+      <div className="projects-container widget">Hello</div>
+      <div className="education-container widget">Hello</div>
     </div>
   );
 };
