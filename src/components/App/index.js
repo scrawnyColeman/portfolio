dfef6998d081ed53e72b10c69ab79278062f6f4d
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-13 19:21:35 +0100

    Basic layout

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div className="display-image-container">Hi</div>
+      <div className="profile-container">Hello</div>
+      <div className="technologies-container">Hello</div>
+      <div className="projects-container">Hello</div>
+      <div className="education-container">Hello</div>
+    </div>
+  );
+};
+
+export default App;
