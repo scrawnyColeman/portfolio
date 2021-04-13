cf753303d76cc345f2f8fbfd771241a7736eb82a
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-13 22:08:13 +0100

    added profile images

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,9 +1,10 @@
+import profileimg from "../../assets/img/profileimg.png";
 import "./style.scss";
 
 const User = () => {
   return (
     <div className="display-image-container widget">
-      <div className="display-image"></div>
+      <img src={profileimg} alt="user" className="display-image" />
       <div className="user-description">
         <span className="user-name">Thomas Hutchinson</span>
         <span className="user-job">Frontend Web Developer</span>
