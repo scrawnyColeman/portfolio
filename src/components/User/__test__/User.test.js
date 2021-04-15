f764e8be5225f31c0eb20142d4f2070203155a57
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-15 12:51:38 +0100

    test user

--- src/components/User/__test__/User.test.js
+++ src/components/User/__test__/User.test.js
@@ -1,9 +1,23 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import User from "..";
 
 describe("<User />", () => {
   test("should render without crashing", () => {
     const { container } = render(<User />);
     expect(container).toBeTruthy();
+  });
+  test("should render user image", () => {
+    render(<User />);
+    expect(screen.getByAltText("user")).toBeInTheDocument();
+  });
+  test("should render name", () => {
+    render(<User />);
+    expect(screen.getByText("Thomas Hutchinson")).toBeInTheDocument();
+  });
+  test("should render slogan", () => {
+    render(<User />);
+    expect(
+      screen.getByText("UI Enthusiast - Programmer - Problem Solver")
+    ).toBeInTheDocument();
   });
 });
