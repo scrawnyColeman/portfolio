2a5284ccf31c806c4f3eae1a67bc1792b6023e5b
scrawnycoleman <thomashutchinson450@gmail.com>
2021-04-15 12:51:26 +0100

    tested profile

--- src/components/Profile/__test__/Profile.test.js
+++ src/components/Profile/__test__/Profile.test.js
@@ -1,9 +1,27 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Profile from "..";
 
 describe("<Profile />", () => {
   test("should render without crashing", () => {
     const { container } = render(<Profile />);
     expect(container).toBeTruthy();
+  });
+  test("should render 3 links", () => {
+    render(<Profile />);
+    const links = screen.getAllByRole("link");
+    expect(links).toHaveLength(3);
+  });
+  test("should render github and linked icon", () => {
+    render(<Profile />);
+    expect(screen.getByAltText("github")).toBeInTheDocument();
+    expect(screen.getByAltText("linkedin")).toBeInTheDocument();
+  });
+  test("should render blurb", () => {
+    render(<Profile />);
+    expect(
+      screen.getByText(
+        "Excited by creative UI's and love to build clean, elegant solutions with modern JavaScript. You'll find me on the web masquerading as @scrawnyColeman"
+      )
+    ).toBeInTheDocument();
   });
 });
